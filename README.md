87952e6f15aa475f2b9f6c0e4505a7a2c5d811de
Samuel <torressam333@gmail.com>
2022-11-10 15:38:37 -0700

    Set up basic express server for posts service

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+# Built with Node and React
+
 This is an example of a blog microservice.
 The UI is not the prettiest but the point of this is to demonstrate
 an async way of having different features work independently as a larger
---
+++ posts/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const { randomBytes } = require('crypto');
+const app = express();
+
+app.use(express.json());
+
+// Only use in memory storage for now
+const posts = {};
+
+app.get('/posts', (req, res) => {
+  res.json(posts);
+});
+
+app.post('/posts', (req, res) => {
+  const id = randomBytes(4).toString('hex');
+  if (!req.body.title)
+    return res.status(400).json({ error: 'Post body cannot be empty' });
+
+  const { title } = req.body;
+
+  posts[id] = {
+    id,
+    title,
+  };
+
+  res.status(201).json(posts[id]);
+});
+
+app.listen(4000, () => console.log('posts server listening on 4000'));
