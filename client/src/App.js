64c558b939f09ed617f109e846247fab5a2ddfcb
Samuel <torressam333@gmail.com>
2022-11-19 10:51:37 -0700

    Add component to list out existing posts

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import PostCreate from './PostCreate';
+import PostList from './PostList';
 
 const App = () => {
   return (
     <div className='container pt-5'>
       <h1>Create Post</h1>
       <PostCreate />
+      <hr className='mb-3 pb-3' />
+      <h1>All Posts</h1>
+      <PostList />
     </div>
   );
 };
---
+++ client/src/PostList.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function PostList() {
+  const [posts, setPosts] = useState({});
+
+  const fetchPosts = async () => {
+    try {
+      const res = await axios.get('http://localhost:4000/posts');
+
+      setPosts(res.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  // Only run once to fetch posts
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  const renderedPosts = Object.values(posts).map((post) => {
+    return (
+      <div
+        className='card'
+        style={{ width: '50%', marginBottom: '20px' }}
+        key={post.id}
+      >
+        <div className='card-body'>
+          <h3>{post.title}</h3>
+        </div>
+      </div>
+    );
+  });
+
+  return (
+    <div className='d-flex flex-row flex-wrap justify-content-between'>
+      {renderedPosts}
+    </div>
+  );
+}
+
+export default PostList;
