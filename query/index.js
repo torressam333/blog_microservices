c33cef616b9e076d5cc70c5e77238f9782311ccc
Samuel <torressam333@gmail.com>
2022-11-22 08:08:09 -0700

    Create events route handler for query service

---
+++ query/index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(express.json());
+app.use(cors({ origin: ['http://localhost:3000'] }));
+
+app.get('/posts', (req, res) => {});
+
+// In memory store for now
+const posts = {};
+
+// Receives events from EB
+app.post('/events', (req, res) => {
+  const { type, data } = req.body;
+
+  if (type === 'PostCreated') {
+    const { id, titile } = data;
+
+    // Insert into post store
+    posts[id] = { id, title, comments: [] };
+  }
+
+  if (type === 'CommentCreated') {
+    const { id, content, postId } = data;
+
+    // Find post from store
+    const post = posts[postId];
+
+    // Add comment to associated post
+    post.comments.push({ id, content });
+  }
+
+  res.status(201).json({ status: 'Success' });
+});
+
+app.listen(4002, () => console.log('Listening on port 4002'));
