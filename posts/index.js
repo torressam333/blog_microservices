c199d4501df1c733fd81aa57c51954736cb92bd8
Samuel <torressam333@gmail.com>
2022-12-10 08:42:23 -0700

    Use unique routes to allow ingress to distinguish pathing

--- posts/index.js
+++ posts/index.js
@@ -14,7 +14,7 @@ app.get('/posts', (req, res) => {
   res.json(posts);
 });
 
-app.post('/posts', async (req, res) => {
+app.post('/posts/create', async (req, res) => {
   const id = randomBytes(4).toString('hex');
   if (!req.body.title)
     return res.status(400).json({ error: 'Post body cannot be empty' });
